41f3ee1acf7d8b9c058afbda3a81aef2123c0487
aliisaro <aliisa.rokala@metropolia.fi>
2023-08-22 14:45:14 +0300

    2.5

---
+++ 2.5.py
@@ -0,0 +1,18 @@
+leiviskamaara = int(input("Anna leiviskät: "))
+naulamaara = int(input("Anna naulat: "))
+luotimaara = float(input("Anna luodit: "))
+
+luodit = luotimaara * 13.3
+naulat = naulamaara * (32 * 13.3)
+leiviskat = leiviskamaara * (20 * 32 * 13.3)
+
+massa = luodit + naulat + leiviskat #grammoina
+
+y = str(massa/1000).split('.')
+
+a = y[0]
+b = y[1]
+
+c = int(b) / 100
+
+print(f"Massa nykymittojen mukaan: {a} kilogrammaa ja {c} grammaa")
