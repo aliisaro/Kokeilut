0b0af8dcf06f3bee8c413c9014db26fef13b564e
aliisaro <aliisa.rokala@metropolia.fi>
2023-08-22 13:49:21 +0300

    2.4

---
+++ 2.4.py
@@ -0,0 +1,9 @@
+luku1 = int(input("Anna 1. luku: "))
+luku2 = int(input("Anna 2. luku: "))
+luku3 = int(input("Anna 3. luku: "))
+
+sum = luku1 + luku2 + luku3
+product = luku1 * luku2 * luku3
+average = sum / 3
+
+print(f"Lukujen summa on {sum}, tulo on {product} ja keskiarvo on {average}")
