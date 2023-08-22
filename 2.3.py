95b1ca30e28bba00a4379ca5f6d5c979d0ff841a
aliisaro <aliisa.rokala@metropolia.fi>
2023-08-22 13:39:04 +0300

    2.3

---
+++ 2.3.py
@@ -0,0 +1,7 @@
+base = int(input("Anna kanta: "))
+height = int(input("Anna korkeus: "))
+
+perimeter = 2 * base + 2 * height
+area = base * height
+
+print(f"Suorakulmion piiri on = {perimeter} ja pinta-ala on {area}")
