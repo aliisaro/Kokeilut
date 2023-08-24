18aa5b83a998db8d0e08348eb9b189c8b6badd7e
aliisaro <aliisa.rokala@metropolia.fi>
2023-08-24 14:06:15 +0300

    3.3

---
+++ 3.3.py
@@ -0,0 +1,22 @@
+sukupuoli = input("Mikä on biologinen sukupuolesi: ")
+arvo = int(input("Mikä on hemoglobiiniarvosi?: "))
+
+if sukupuoli == "nainen":
+    if arvo > 175:
+        print("Hemoglobiini arvosi on korkea.")
+    elif arvo < 117:
+        print("Hemoglobiiniarvosi on alhainen.")
+    else:
+        print("Hemoglobiiniarvosi on normaali.")
+
+    print("Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.")
+
+elif sukupuoli == "mies":
+    if arvo > 195:
+        print("Hemoglobiini arvosi on korkea.")
+    elif arvo < 134:
+        print("Hemoglobiiniarvosi on alhainen.")
+    else:
+        print("Hemoglobiiniarvosi on normaali.")
+
+    print("Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.")
