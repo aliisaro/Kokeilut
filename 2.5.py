6d90931db255276f6b49a1f0e9fba11502554a16
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-01 13:32:14 +0300

    2.5

--- 2.5.py
+++ 2.5.py
@@ -8,4 +8,7 @@ leiviskat = leiviskamaara * (20 * 32 * 13.3)
 
 massa = luodit + naulat + leiviskat #grammoina
 
-print(f"Massa nykymittojen mukaan: {massa} grammaa")
+kg = int(massa//1000)
+g = massa-1000*kg
+
+print(f"Massa nykymittojen mukaan: {kg} kilogrammaa ja {g:.2f} grammaa.")
