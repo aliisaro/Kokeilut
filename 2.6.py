0d372cfdea5bf998c9cf06fa8b3d4d94812e1597
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-01 13:36:33 +0300

    2.6

--- 2.6.py
+++ 2.6.py
@@ -3,7 +3,7 @@ x = 0
 code1 = ""
 
 while (x < 3) :
-    number = str(random.randrange(0,9))
+    number = str(random.randrange(0,10))
     code1 = code1 + number
     x +=1
 
@@ -13,7 +13,7 @@ x = 0
 code2 = ""
 
 while (x < 4) :
-    number = str(random.randrange(1,6))
+    number = str(random.randrange(1,7))
     code2 = code2 + number
     x +=1
 
