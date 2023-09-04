7954adf782b9d98b5705e46ad0e1156d40a423d0
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-04 10:45:49 +0300

    6.5

---
+++ 6.5.py
@@ -0,0 +1,15 @@
+def listat(kokonaisluvut):
+
+    karsittu = []
+    for num in kokonaisluvut:
+        if num % 2 == 0:
+            karsittu.append(num)
+
+    return karsittu
+
+
+karsitaan = [1,2,3,4,5,6,7,8,9,10]
+listat(karsitaan)
+print(karsitaan)
+print(listat(karsitaan))
+
