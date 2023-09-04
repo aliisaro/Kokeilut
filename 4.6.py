1d2e8d195f047e01ffd4fd77178ab52bfffbcc92
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-04 09:46:26 +0300

    4.6

---
+++ 4.6.py
@@ -0,0 +1,22 @@
+import random
+
+n = 0 #ympyrän sisälle jäävät pisteet
+
+#kysytään pisteiden lukumäärää
+N = int(input("Anna pisteiden määrä: "))
+
+for luku in range(N):
+
+    #arvotaan piste nelion sisältä
+    x = random.randrange(-1,1)
+    y = random.randrange(-1,1)
+
+    #lasketaan jos piste on nelion sisällä olevan ympyrän sisällä
+    if x^2+y^2<1:
+        n += 1
+
+#lasketaan ja tulostetaan piin likiarvo
+approx = sum([4/i - 4/(i+2) for i in range(1, 2*n+1, 4)])
+print(approx)
+
+#print(pii≈4n/N)
