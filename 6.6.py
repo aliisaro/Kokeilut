9fd4f083b0936943b2c25803291695a881e87a06
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-08 14:03:37 +0300

    6.6

--- 6.6.py
+++ 6.6.py
@@ -1,29 +1,24 @@
+import math
 def pizza(halkaisija, hinta):
 
-    area = 3.14159 * pow((halkaisija / 2), 2)
+    area = ((math.pi * halkaisija) / 100) / 100
 
-    if hinta == hinta1 and halkaisija == halkaisija1:
-        yksikkohinta1 = hinta / area
-        yksikkohinnat.append(yksikkohinta1)
-    else:
-        yksikkohinta2 = hinta / area
-        yksikkohinnat.append(yksikkohinta2)
+    yksikkohinta = hinta / area
 
-        print(f"\nEkan pizzan yksikkohinta on {yksikkohinnat[0]:.3f} e/cm2.")
-        print(f"Tokan pizzan yksikkohinta on {yksikkohinnat[1]:.3f} e/cm2.")
-
-        if yksikkohinnat.index(min(yksikkohinnat)) == 0:
-            print(f"Eka pizza antaa paremman vastineen rahalle.")
-        elif yksikkohinnat.index(min(yksikkohinnat)) == 1:
-            print(f"Toka pizza antaa paremman vastineen rahalle.")
-
-yksikkohinnat = []
+    return yksikkohinta
 
 hinta1 = float(input("Ekan pizzan hinta (e): "))
 halkaisija1 = float(input("Ekan pizzan halkaisija (cm): "))
 pizza(halkaisija1, hinta1)
+eka = pizza(halkaisija1, hinta1)
 
 hinta2 = float(input("Tokan pizzan hinta (e): "))
 halkaisija2 = float(input("Tokan pizzan halkaisija (cm): "))
 pizza(halkaisija2, hinta2)
+toka = pizza(halkaisija2, hinta2)
 
+
+if eka < toka:
+    print(f"Eka pizza antaa paremman vastineen rahalle.")
+else:
+    print(f"Toka pizza antaa paremman vastineen rahalle.")
