a16a56d6a14cebe9f33bc0cdd8051ff800b7401a
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-18 10:48:00 +0300

    8.1

--- 8.1.py
+++ 8.1.py
@@ -1,1 +1,24 @@
-moi
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host = '127.0.0.1',
+    port = 3306,
+    database = 'flight_game',
+    user = 'root',
+    password = '1234',
+    autocommit = True
+)
+
+def LentokenttaNimi(koodi):
+    sql = "SELECT name, municipality FROM airport WHERE ident = '" + koodi + "'"
+
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+
+    if kursori.rowcount >0:
+        for rivi in tulos:
+            print(f"Lentokentän nimi on '{rivi[0]}' ja sijaintikunta on '{rivi[1]}'.")
+
+ICAO = input("Anna lentokentän ICAO-koodi: ")
+LentokenttaNimi(ICAO)
