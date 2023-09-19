5c38f5046c89d1be16265b6e9602c2ce0b313257
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-19 12:35:48 +0300

    8.3

--- 8.3.py
+++ 8.3.py
@@ -10,37 +10,21 @@ yhteys = mysql.connector.connect(
     autocommit = True
 )
 
-def LentokenttienEtaisyys(koodi1, koodi2):
-    sql1 = "SELECT latitude_deg, longitude_deg FROM airport "
-    sql1 += "WHERE ident = '" + koodi1 + "'"
-
-
-    kursori = yhteys.cursor()
-    kursori.execute(sql1)
-    tulos = kursori.fetchall()
-
-    if kursori.rowcount >0:
-        for rivi in tulos:
-            paikka1 = (rivi[0], rivi[1])
-
-    sql2 = "SELECT latitude_deg, longitude_deg FROM airport "
-    sql2 += "WHERE ident = '" + koodi2 + "'"
+def LentokenttienEtaisyys(ICAO1, ICAO2):
+    sql = "SELECT latitude_deg, longitude_deg FROM airport "
+    sql += "WHERE ident = '" + ICAO1 + "'"
+    sql += "OR ident = '" + ICAO2 + "'"
 
     kursori = yhteys.cursor()
-    kursori.execute(sql2)
+    kursori.execute(sql)
     tulos = kursori.fetchall()
 
-    if kursori.rowcount > 0:
-        for rivi in tulos:
-            paikka2 = (rivi[0], rivi[1])
+    #print(tulos[0], tulos[1])
 
-    print(f"Lentokenttien välinen matka kilometreinä: {distance.distance(paikka1, paikka2).kilometers:0.3f}")
+    print(f"Lentokenttien välinen matka kilometreinä: {distance.distance(tulos[0], tulos[1]).km:0.2f}")
+    return
 
 ICAO1 = input("Anna ensimmäisen lentokentän ICAO-koodi: ")
 ICAO2 = input("Anna toisen lentokentän ICAO-koodi: ")
 LentokenttienEtaisyys(ICAO1, ICAO2)
 
-
-#YKSI SQL-LAUSE
-#sql = "SELECT latitude_deg, longitude_deg FROM airport "
-#sql += "WHERE ident IN ('" + koodi1 + "', '" + koodi2 + "')"
