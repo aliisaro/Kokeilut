036ecf494d79af074d1721cda832e80d645d41b7
aliisaro <aliisa.rokala@metropolia.fi>
2023-09-22 13:30:39 +0300

    4.2

--- 4.2.py
+++ 4.2.py
@@ -1,8 +1,7 @@
-while True:
-    tuumat = float(input("Anna tuumat: "))
+tuumat = 1
 
-    if tuumat >= 0:
-        sentit = tuumat * 2.54
-        print(f"{tuumat} tuumaa senttimetreinä on {sentit}")
-    else:
-        break
+while tuumat > 0:
+    tuumat = float(input("Anna tuumamäärä: "))
+    sentteina = tuumat * 2.54
+    if tuumat > 0:
+        print(f"Tuumat sentteinä: {sentteina}")
