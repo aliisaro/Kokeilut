0c0d9a0067e82ba94ab3ec0460840ba4b492b4ac
aliisaro <aliisa.rokala@metropolia.fi>
2023-10-24 22:54:05 +0300

    10.1

---
+++ 10.1.py
@@ -0,0 +1,35 @@
+class Hissi:
+    def __init__(self, alin, ylin):
+        self.alin = alin
+        self.ylin = ylin
+
+    viimeisin = [1]
+
+    def siirry_kerrokseen(self, kerros):
+        self.kerros = kerros
+
+        if self.kerros > int(self.viimeisin[-1]):
+            while self.kerros >= int(self.viimeisin[-1]):
+                print(f"Olet kerroksessa {int(self.viimeisin[-1])}")
+                self.kerros_ylos(1);
+        elif self.kerros < int(self.viimeisin[-1]):
+            while self.kerros < int(self.viimeisin[-1]):
+                self.kerros_alas(1);
+                print(f"Olet kerroksessa {int(self.viimeisin[-1])}")
+
+    def kerros_ylos(self, ylos):
+        self.luku = int(self.viimeisin[-1])
+        self.luku += ylos
+        self.viimeisin.append(self.luku)
+
+    def kerros_alas(self, alas):
+        self.luku = int(self.viimeisin[-1])
+        self.luku -= alas
+        self.viimeisin.append(self.luku)
+
+
+h = Hissi(1, 7)
+
+h.siirry_kerrokseen(5)
+print("\n")
+h.siirry_kerrokseen(1)
