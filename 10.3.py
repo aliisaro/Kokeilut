49cb0801f2371f57c6303bf4797d26238febbc4f
aliisaro <aliisa.rokala@metropolia.fi>
2023-10-25 14:05:46 +0300

    10.3

--- 10.3.py
+++ 10.3.py
@@ -4,7 +4,6 @@ class Hissi:
         self.ylin = ylin
         self.kerros = alin
 
-
     def siirry_kerrokseen(self, uusikerros):
 
         while self.kerros != uusikerros:
@@ -24,7 +23,6 @@ class Hissi:
         if self.kerros > self.alin:
             self.kerros -= 1
 
-#PÄÄOHJELMA
 class Talo:
     def __init__(self, alin, ylin, lukumaara):
         self.alin = alin
@@ -36,7 +34,6 @@ class Talo:
         print(f"\nAjat hissiä {numero}")
         self.hissit[numero-1].siirry_kerrokseen(kohdekerros)
 
-
     def palohälytys(self):
         luku = 1
         for hissi in self.hissit:
@@ -44,65 +41,9 @@ class Talo:
             hissi.kerros = self.hissit[0].alin
             print(f"Hissi on {luku} siirtyi kerrokseen {hissi.kerros}.\n")
             luku += 1
-
-
-talo = Talo(1, 7 , 3)
-
-talo.aja_hissia(1, 7)
-talo.aja_hissia(2, 3)
-talo.aja_hissia(3, 2)
-print("\n")
-talo.palohälytys()
-class Hissi:
-    def __init__(self, alin, ylin):
-        self.alin = alin
-        self.ylin = ylin
-        self.kerros = alin
-
-
-    def siirry_kerrokseen(self, uusikerros):
-
-        while self.kerros != uusikerros:
-            print(f"Olet kerroksessa {self.kerros}")
-
-            if self.kerros < uusikerros:
-                self.kerros_ylos();
-            elif self.kerros > uusikerros:
-                self.kerros_alas();
-
-        print(f"Olet kerroksessa {self.kerros}")
-    def kerros_ylos(self):
-        if self.kerros < self.ylin:
-            self.kerros += 1
-
-    def kerros_alas(self):
-        if self.kerros > self.alin:
-            self.kerros -= 1
 
 #PÄÄOHJELMA
-class Talo:
-    def __init__(self, alin, ylin, lukumaara):
-        self.alin = alin
-        self.ylin = ylin
-        self.lukumaara = lukumaara
-        self.hissit = [Hissi(alin, ylin) for i in range(lukumaara)]
-
-    def aja_hissia(self, numero, kohdekerros):
-        print(f"\nAjat hissiä {numero}")
-        self.hissit[numero-1].siirry_kerrokseen(kohdekerros)
-
-
-    def palohälytys(self):
-        luku = 1
-        for hissi in self.hissit:
-            print(f"Palohälytys!!! Hissi on {luku}  kerroksesssa {hissi.kerros}.")
-            hissi.kerros = self.hissit[0].alin
-            print(f"Hissi on {luku} siirtyi kerrokseen {hissi.kerros}.\n")
-            luku += 1
-
-
 talo = Talo(1, 7 , 3)
-
 talo.aja_hissia(1, 7)
 talo.aja_hissia(2, 3)
 talo.aja_hissia(3, 2)
