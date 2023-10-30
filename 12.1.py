1cc9ca7a581a3453c31177a42da9d804f7be3324
aliisaro <aliisa.rokala@metropolia.fi>
2023-10-30 12:43:40 +0200

    12.1

---
+++ 12.1.py
@@ -0,0 +1,7 @@
+import json
+import requests
+
+pyyntö = "https://api.chucknorris.io/jokes/random"
+vastaus = requests.get(pyyntö).json()
+#print(json.dumps(vastaus, indent=2))
+print(vastaus["value"])
